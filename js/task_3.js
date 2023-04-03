887269b5d2219891405f01aa230da5996fa4ac46
Ruslan Zulufov <gothiq2302@gmail.com>
2023-04-04 01:39:38 +0600

    Доработка третьего задания и реализация бонусного

--- js/task_3.js
+++ js/task_3.js
@@ -1,9 +1,17 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#container')
+const color = []
+
+function randomColorHex() {
+    let hex = Math.floor(Math.random() * 16777215).toString(16);
+    color.push(`#${hex}`)
+}
+
 for (let i = 1; i <= 5; i++) {
-	let div = document.createElement('div');
-	div.className = 'element';
-	div.textContent = 'Element ' + i;
-	container.appendChild(div);
+    randomColorHex()
+    let div = document.createElement('div')
+        div.classList.add('element')
+        div.innerHTML = `Element <span style="color: ${color[i-1]};">${i}</span>`
+
+    container.append(div)
 }
 console.log(container)
-
